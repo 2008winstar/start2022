a437d75cba80d897e98a7ab818a3db54db3bb7a3
winstar <2008winstar@sina.com>
2022-01-15 12:28:43 +0800

    add leetcode code

--- leetcode/429.js
+++ leetcode/429.js
@@ -17,4 +17,25 @@
 const levelOrder = function(root) {
   const result = [];
 
+  if (root) {
+    const arr = [root];
+    while (arr.length) {
+      const len = arr.length;
+      const level = [];
+
+      for (let i = 0; i < len; i++) {
+        const node = arr.shift();
+        level.push(node.val);
+        if (node.children) {
+          node.children.forEach((item) => {
+            arr.push(item);
+          });
+        }
+      }
+
+      result.push(level);
+    }
+  }
+
+  return result;
 };
